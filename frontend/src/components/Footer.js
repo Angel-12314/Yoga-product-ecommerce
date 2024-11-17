5005b7f053c13a65bb7fedd0778d8fe1ca92829c
Angel-12314 <59677750+Angel-12314@users.noreply.github.com>
2024-11-17 14:50:31 +0530

    Add files via upload

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return <footer className="py-1 bg-primary">
+    <p className="text-center text-white mt-1 ">
+      Mini-cart - 2023-2024, All Rights Reserved
+    </p>
+      </footer>
+}
